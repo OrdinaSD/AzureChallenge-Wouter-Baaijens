a316e8702b92be556b4c4371c5a3f6151228b766
Unknown <wouter.baaijens@ordina.nl>
2017-03-02 19:58:15 +0000

    First commit

---
+++ wouterbaaijensSN/wouterbaaijensSN.web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(wouterbaaijensSN.web.Startup))]
+namespace wouterbaaijensSN.web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
